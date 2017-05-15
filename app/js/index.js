03c4f05c31778f66f7bf9feed1240c1f3a43d3a9
Isrenx <victorgrxiang@gmail.com>
2017-05-15 16:04:27 +0800

    2017-05-15-16:00-增加loading页

--- app/js/index.js
+++ app/js/index.js
@@ -4,6 +4,32 @@
 import Preload from './Preload';
 import {$$} from './func';
 const resources = [
+    '../images/banner-01.jpg',
+    '../images/banner-02.jpg',
+    '../images/banner-03.jpg',
+    '../images/banner-04.jpg',
+    '../images/banner-05.jpg',
+    '../images/banner-06.jpg',
+    '../images/banner-07.jpg',
+    '../images/banner-08.jpg',
+    '../images/banner-09.jpg',
+    '../images/banner-10.jpg',
+    '../images/banner-11.jpg',
+    '../images/banner.jpg',
+    '../images/news.jpg',
+    '../images/s-01.png',
+    '../images/s-02.png',
+    '../images/s-03.png',
+    '../images/s-04.png',
+    '../images/s-05.png',
+    '../images/s-06.png',
+    '../images/slide-01.jpg',
+    '../images/stu-01.jpg',
+    '../images/stu-02.jpg',
+    '../images/stu-03.jpg',
+    '../images/stu-04.jpg',
+    '../images/wechat.jpg',
+    '../images/weibo.jpg',
 ]
 
 let loader = new Preload (
@@ -19,10 +45,15 @@ let loader = new Preload (
             var a = 0;
             function progress(){
                 a++;
-                // $$('.progess').innerHTML = a;
+                $$('.precent-num span').innerHTML = a + '%';
+                $$('.car-box').style.width = a + '%';
+                $$('.precent-line span').style.width = a + '%';
                 if(a>=100){
                     a=100;
-                                       
+                    $$('.loading').classList.remove('ZindexMax'); 
+                    $$('.loading').classList.add('indexMin');
+                    $$('.p1 .news-box .hot-news').classList.add('current');           
+                    $$('.p1 .news-box .school-news').classList.add('current');
                     return;
                 }
                 requestAnimFrame(progress);
